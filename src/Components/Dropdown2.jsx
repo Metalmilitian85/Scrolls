654fa56d1eb757b8e34eb842689623f7a405cb3b
Tim <timward1985@gmail.com>
2023-06-26 23:15:59 -0500

    fonts

--- src/Components/Dropdown2.jsx
+++ src/Components/Dropdown2.jsx
@@ -26,7 +26,7 @@ export default function Dropdown() {
             </button>
 
             {isOpen && (
-                <div className="bg-black absolute top-20 flex flex-col items-start border-solid rounded border-4 border-gray-400 p-2 w-[150px]">
+                <div className="bg-black absolute top-[55px] flex flex-col items-start border-solid rounded border-4 border-gray-400 p-2 w-[150px]">
                     {books.map((item) => (
                         <div className="flex w-full justify-between p-2 hover:bg-gray-700 cursor-pointer rounded-r-lg border-l-transparent hover:border-l-white border-l-4">
                             <h3 className="font-bold">{item.type}</h3>
