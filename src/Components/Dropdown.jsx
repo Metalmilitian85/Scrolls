673f22d708ed4b87278d04534019a47c420843d4
Tim <timward1985@gmail.com>
2023-06-28 21:11:08 -0500

    navbar

--- src/Components/Dropdown.jsx
+++ src/Components/Dropdown.jsx
@@ -9,15 +9,13 @@ export default function Dropdown() {
     const [ isOpen, setIsOpen ] = useState(false);
 
     const handleOpen = () => {
-        setIsOpen(!isOpen)
-    }
+            setIsOpen(!isOpen)
+        }
 
-    /* const closeMenu = () => {
-        const closeDropdown = setIsOpen(!false)
-        document.addEventListener('mousedown', closeDropdown)
+    const handleClose = () => {
+            setIsOpen(isOpen)
     }
-    */
-
+    
     return (
         <div className="mx-auto relative flex flex-col w-full bg-none z-10" >
             <button onClick={handleOpen} className="bg-none p-2 w-[120px] flex items-center justify-between font-bold text-lg rounded-lg tracking-wider border-4 border-transparent active:border-white duration-300 active:text-white"
@@ -36,11 +34,11 @@ export default function Dropdown() {
                 <div className="bg-black absolute left-[150px] flex flex-col items-start border-solid rounded border-4 border-gray-400 p-2 w-[150px]" >
                         <div className="flex w-full justify-between p-2">
                             <ul>
-                                <li className=" hover:bg-gray-700 cursor-pointer rounded-r-lg border-l-transparent hover:border-l-white border-l-4"><Link to="/Arena">Arena</Link></li>
-                                <li className=" hover:bg-gray-700 cursor-pointer rounded-r-lg border-l-transparent hover:border-l-white border-l-4"><Link to="/Daggerfall">Daggerfall</Link></li>
-                                <li className=" hover:bg-gray-700 cursor-pointer rounded-r-lg border-l-transparent hover:border-l-white border-l-4"><Link to="/Morrowind">Morrowind</Link></li>
-                                <li className=" hover:bg-gray-700 cursor-pointer rounded-r-lg border-l-transparent hover:border-l-white border-l-4"><Link to="/Oblivion">Oblivion</Link></li>
-                                <li className=" hover:bg-gray-700 cursor-pointer rounded-r-lg border-l-transparent hover:border-l-white border-l-4"><Link to="/Skyrim">Skyrim</Link></li>
+                                <li onClick={handleOpen} className=" hover:bg-gray-700 cursor-pointer rounded-r-lg border-l-transparent hover:border-l-white border-l-4"><Link to="/Arena">Arena</Link></li>
+                                <li onClick={handleOpen} className=" hover:bg-gray-700 cursor-pointer rounded-r-lg border-l-transparent hover:border-l-white border-l-4"><Link to="/Daggerfall">Daggerfall</Link></li>
+                                <li onClick={handleOpen} className=" hover:bg-gray-700 cursor-pointer rounded-r-lg border-l-transparent hover:border-l-white border-l-4"><Link to="/Morrowind">Morrowind</Link></li>
+                                <li onClick={handleOpen} className=" hover:bg-gray-700 cursor-pointer rounded-r-lg border-l-transparent hover:border-l-white border-l-4"><Link to="/Oblivion">Oblivion</Link></li>
+                                <li onClick={handleOpen} className=" hover:bg-gray-700 cursor-pointer rounded-r-lg border-l-transparent hover:border-l-white border-l-4"><Link to="/Skyrim">Skyrim</Link></li>
                             </ul>
                         </div>
                 </div>
