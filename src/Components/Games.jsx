e1f74dd883d3fdbf17a05df505f62055943d85d1
Tim <timward1985@gmail.com>
2023-07-03 12:55:02 -0500

    menu

--- src/Components/Games.jsx
+++ src/Components/Games.jsx
@@ -9,13 +9,13 @@ export default function Games() {
     return (
         <div id="/History" className="bg-black py-10">
           <div className="nav2">
-            <h3 className="text-white text-3xl text-center pb-5">GAAAAAAMES</h3>
-            <ul className="text-white text-center flex justify-center space-x-7 mb-5">
-                <a href="#Arena"><li><h3 className="hover:underline hover:cursor-pointer decoration-solid decoration-yellow-500 decoration-2">Arena</h3></li></a>
-                <a href="#Daggerfall"><li><h3 className="hover:underline hover:cursor-pointer decoration-solid decoration-yellow-500 decoration-2">Daggerfall</h3></li></a>
-                <a href="#Morrowind"><li><h3 className="hover:underline hover:cursor-pointer decoration-solid decoration-yellow-500 decoration-2">Morrowind</h3></li></a>
-                <a href="#Oblivion"><li><h3 className="hover:underline hover:cursor-pointer decoration-solid decoration-yellow-500 decoration-2">Oblivion</h3></li></a>
-                <a href="#Skyrim"><li><h3 className="hover:underline hover:cursor-pointer decoration-solid decoration-yellow-500 decoration-2">Skyrim</h3></li></a>
+            <h3 className="text-white text-xl text-center pb-1">GAAAAAAMES</h3>
+            <ul className="text-white text-center flex justify-center space-x-6 mb-5">
+                <a href="#Arena"><li><h3 className="text-sm hover:underline hover:cursor-pointer decoration-solid decoration-yellow-500 decoration-2">Arena</h3></li></a>
+                <a href="#Daggerfall"><li><h3 className="text-sm hover:underline hover:cursor-pointer decoration-solid decoration-yellow-500 decoration-2">Daggerfall</h3></li></a>
+                <a href="#Morrowind"><li><h3 className="text-sm hover:underline hover:cursor-pointer decoration-solid decoration-yellow-500 decoration-2">Morrowind</h3></li></a>
+                <a href="#Oblivion"><li><h3 className="text-sm hover:underline hover:cursor-pointer decoration-solid decoration-yellow-500 decoration-2">Oblivion</h3></li></a>
+                <a href="#Skyrim"><li><h3 className="text-sm hover:underline hover:cursor-pointer decoration-solid decoration-yellow-500 decoration-2">Skyrim</h3></li></a>
             </ul>
           </div>
             <div class="history" className="flex-col" >
